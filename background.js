17bb27215fd9e33404d3155442b222819727f781
zs <lightondust@gmail.com>
2020-01-28 15:45:10 +0900

    record url to local storage

--- background.js
+++ background.js
@@ -6,3 +6,20 @@ chrome.contextMenus.create({
       console.log('menu button triggered');
   }
 });
+
+chrome.tabs.onUpdated.addListener( function( tabId,  changeInfo,  tab) {
+    chrome.storage.local.get(tab.url, function(rec_old){
+        let contents = {};
+        contents[tab.url] = {
+            'title': tab.title,
+            'time': new Date(),
+            'url': tab.url
+        };
+        if(!rec_old.count){
+            contents['count'] = 1;
+        }else{
+            contents['count'] = rec_old.count+1;
+        }
+        chrome.storage.local.set(contents, function(){});
+    });
+});
