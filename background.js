e0524f2d6dd61a0c7552279859429a9c60d097af
zs <lightondust@gmail.com>
2020-01-28 19:19:10 +0900

    record urls on specified domains

--- background.js
+++ background.js
@@ -1,3 +1,12 @@
+let url_domain_list = [
+    "qiita.com",
+    "github.com",
+    "arxiv.org",
+    "scholar.google.com",
+    "stackoverflow.com",
+    "medium.com"
+];
+
 chrome.contextMenus.create({
   "title" : "このページを登録する(まだ工事中)",
   "type"  : "normal",
@@ -9,17 +18,27 @@ chrome.contextMenus.create({
 
 chrome.tabs.onUpdated.addListener( function( tabId,  changeInfo,  tab) {
     chrome.storage.local.get(tab.url, function(rec_old){
-        let contents = {};
-        contents[tab.url] = {
-            'title': tab.title,
-            'time': new Date(),
-            'url': tab.url
-        };
-        if(!rec_old.count){
-            contents['count'] = 1;
-        }else{
-            contents['count'] = rec_old.count+1;
+        let url = tab.url;
+        let time = new Date();
+        if(url_domain_filter(url)){
+            let contents = {};
+            contents[tab.url] = {
+                'title': tab.title,
+                'time': time.toLocaleString(),
+                'url': tab.url
+            };
+            if(!rec_old.count){
+                contents[tab.url]['count'] = 1;
+            }else{
+                contents[tab.url]['count'] = rec_old.count+1;
+            }
+            chrome.storage.local.set(contents, function(){});
         }
-        chrome.storage.local.set(contents, function(){});
     });
 });
+
+function url_domain_filter(url){
+    let url_obj = new URL(url);
+    let hostname = url_obj.hostname;
+    return url_domain_list.includes(hostname);
+}
